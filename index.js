9b9da7e7a416956f460d69c4c036a13ffad5461f
mark <mddoyne@asu.edu>
2022-04-13 14:13:06 -0600

    save

---
+++ index.js
@@ -0,0 +1,39 @@
+function shout(string)
+{
+    return string.toUpperCase();
+}
+
+function whisper(string)
+{
+    return string.toLowerCase();
+}
+
+function logShout(string)
+{
+    console.log(string.toUpperCase());
+}
+
+function logWhisper(string)
+{
+    console.log(string.toLowerCase());
+}
+
+
+
+function sayHiToHeadphonedRoommate(string)
+{
+    if(string = string.toLowerCase())
+    {
+        console.log("I can't hear you!");
+    }
+
+    else if(string = string.toUpperCase())
+    {
+        console.log("YES INDEED!");
+    }
+
+    else if(string = "Let's have dinner together!")
+    {
+        console.log("I would love to!");
+    }
+}
